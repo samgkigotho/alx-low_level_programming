82a74992f8a6f58ce25638efa467d7504080b47b
samgkigotho <samgatambia@gmail.com>
2022-12-08 17:28:00 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  * Return: Always 0 (Success)                                                                                                            */                                                                                                                                     
 int main(void)                                                                                                                          
 {                                                                                                                                       
-	        puts("\"Programming is like building a multi lingual puzzle");
+	        puts("\"Programming is like building a multi lingual puzzle);
                 return (0); 
 
 }	
