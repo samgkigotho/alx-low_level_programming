46198e24eab1371c1c200e68c1c8630086429f7d
samgkigotho <samgatambia@gmail.com>
2022-12-14 17:00:03 -0800

    ./1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_alphabet - This program prints the english alphabets a-z
+ *Return: void
+ */
+void print_alphabet(void)
+
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+
+}
