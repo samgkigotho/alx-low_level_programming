d735eef10cea45353d216ba15a0ad6ef81a3fc4c
samgkigotho <samgatambia@gmail.com>
2022-12-22 18:11:23 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 
+/**
+ * main - entry block
+ * Return: 0
+ **/
 int main(void)
-
 {
 
-int n;
+int i, j;
 
-int a[5];
-
-int *p;
-
-a[2] = 1024;
-
-p = &n;
-
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-
-*(p + 5) = 98;
-
-/* ...so that this prints 98\n */
-
-printf("a[2] = %d\n", a[2]);
-
+for (i = 0; i < 100; i++)
+{
+for (j = i + 1; j < 100; j++)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
 return (0);
 }
